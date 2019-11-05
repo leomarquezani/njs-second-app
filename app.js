db3886ca90086257e2a505a577ca7d8b23874067
Leonardo Marquezani <leomarquezani@gmail.com>
2019-11-05 14:06:48 -0300

    Updated

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 
 const app = express();
 
 //add parser before the routes
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/add-product', (req, res) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.use('/product', (req, res) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello</h1>')
-});
-
-app.listen(3000);
+app.listen(3001);
