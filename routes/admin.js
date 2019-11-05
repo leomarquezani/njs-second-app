6798d6c004960d92edee621aac442239efa57c37
Leonardo Marquezani <leomarquezani@gmail.com>
2019-11-05 18:02:00 -0300

    Updated

--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,17 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res) => {
+  products.push({ title: req.body.title })
   console.log(req.body);
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
