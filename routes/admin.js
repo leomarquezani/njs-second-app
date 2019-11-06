8ab291340e6ac1e98a74451338753b5f163a0c37
Leonardo Marquezani <leomarquezani@gmail.com>
2019-11-06 17:57:51 -0300

    Finished Pug

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,9 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product', (req, res) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' })
+  //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res) => {
--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,7 @@ router.get('/', (req, res, next) => {
 
   //render the template file
   //the template engine(pug, ejs, ...) is defined in app.js property view engine
-  res.render('shop', { prods: adminData.products, docTitle: 'Shop' });
+  res.render('shop', { prods: adminData.products, pageTitle: 'Shop', path: '/' });
 
   //send html files to the borwser, needs to construct the path hence the rootDir property
   //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
