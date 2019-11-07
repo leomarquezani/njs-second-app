5396842ae8f60ac0659ece985b6be966e84d58e1
Leonardo Marquezani <leomarquezani@gmail.com>
2019-11-06 21:08:55 -0300

    Added handlebars

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.set('view engine', 'pug');//set the template engine with express property view engine
+app.engine(
+  "hbs",
+  expressHbs({
+    layoutsDir: "views/layouts",
+    defaultLayout: "main-layout",
+    extname: "hbs"
+  })
+); //for handlebar we need to create a new engine
+app.set("view engine", "hbs");
+//app.set('view engine', 'pug');//set the template engine with express property view engine
 
 //add parser before the routes
-app.use(bodyParser.urlencoded({extended: false}));//dep that helps parsing
-app.use(express.static(path.join(__dirname, 'public')));//to use static content like css set this propety and the folder 
+app.use(bodyParser.urlencoded({ extended: false })); //dep that helps parsing
+app.use(express.static(path.join(__dirname, "public"))); //to use static content like css set this propety and the folder
 
-app.use('/admin', adminData.routes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
   //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,23 +1,28 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
 const products = [];
 
-router.get('/add-product', (req, res) => {
-  
-  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' })
+router.get("/add-product", (req, res) => {
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    activeAddProduct: true,
+    productCSS: true,
+    formsCSS: true
+  });
   //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/add-product', (req, res) => {
-  products.push({ title: req.body.title })
+router.post("/add-product", (req, res) => {
+  products.push({ title: req.body.title });
   console.log(req.body);
-  res.redirect('/');
+  res.redirect("/");
 });
 
 exports.routes = router;
