01916dffdac95ac63ada223ee2e47a3b321c0b3a
Leonardo Marquezani <leomarquezani@gmail.com>
2019-11-08 13:08:27 -0300

    Second App

--- app.js
+++ app.js
@@ -1,36 +1,24 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
-
-const adminData = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-/*app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts",
-    defaultLayout: "main-layout",
-    extname: "hbs"
-  })
-);*/ //for handlebar we need to create a new engine
-//app.set("view engine", "hbs");
-app.set("view engine", "ejs");
-//app.set('view engine', 'pug');//set the template engine with express property view engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-//add parser before the routes
-app.use(bodyParser.urlencoded({ extended: false })); //dep that helps parsing
-app.use(express.static(path.join(__dirname, "public"))); //to use static content like css set this propety and the folder
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use("/admin", adminData.routes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-  //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,26 +1,22 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
 
-const rootDir = require("../util/path");
-const adminData = require("./admin");
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  //render the template file
-  //the template engine(pug, ejs, ...) is defined in app.js property view engine
-  res.render("shop", {
-    prods: adminData.products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: adminData.products.length > 0,
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
     activeShop: true,
     productCSS: true
   });
-
-  //send html files to the borwser, needs to construct the path hence the rootDir property
-  //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
